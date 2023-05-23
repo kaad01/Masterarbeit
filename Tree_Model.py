c524dc33976ff0e5ada0c7ca06337098f7e5574e
Kaan Dönmez <k-t-d@live.de>
2023-05-23 23:09:03 +0200

    einzeln

--- Tree_Model.py
+++ Tree_Model.py
@@ -13,6 +13,8 @@ from torch_geometric.data import Batch
 import matplotlib.pyplot as plt
 import pickle
 from sklearn.metrics import roc_auc_score, confusion_matrix, accuracy_score, RocCurveDisplay
+import random
+import math
 
 def save(object, name):
     with open(name, 'wb') as f:
@@ -35,15 +37,21 @@ tree_loader = load("tree_loader_simple.pkl")
 
 print("TreeLoader loaded")
 train_dataset, valid_dataset = tree_loader.split()
+train_dataset = list(train_dataset)
+valid_dataset = list(valid_dataset)
+print(type(train_dataset))
+
+random.shuffle(train_dataset)
+random.shuffle(valid_dataset)
 
 
-def hetero_collate(data):
-    return Batch.from_data_list(data)
+# def hetero_collate(data):
+#     return Batch.from_data_list(data)
     
 
-# Erstellen von DataLoadern für jeden Datensatz
-train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, collate_fn=hetero_collate)
-valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=True, collate_fn=hetero_collate)
+# # Erstellen von DataLoadern für jeden Datensatz
+# train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, collate_fn=hetero_collate)
+# valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=True, collate_fn=hetero_collate)
 
 
 
@@ -110,8 +118,14 @@ class Model(torch.nn.Module):
         return x
 
     def decode(self, z, pos_edge_index, neg_edge_index):
-        pos_pred = self.classifier(z['module'], pos_edge_index)
-        neg_pred = self.classifier(z['module'], neg_edge_index)
+        try:
+            pos_pred = self.classifier(z['module'], pos_edge_index)
+        except:
+            pos_pred = torch.tensor([0])
+        try:
+            neg_pred = self.classifier(z['module'], neg_edge_index)
+        except:
+            neg_pred = torch.tensor([0])
         return pos_pred, neg_pred
 
 
@@ -128,14 +142,15 @@ def generate_positive_negative_examples(edge_index, num_nodes):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Model(hidden_channels=64).to(device)
-model.load_state_dict(torch.load('models/model_real_good.pt')['model_state_dict'])
+# model.load_state_dict(torch.load('models/model_real_good.pt')['model_state_dict'])
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 
 def train():
     model.train()
 
     total_loss = 0
-    for data in tqdm(train_dataloader):
+    skipped = 0
+    for data in tqdm(train_dataset):
         data = data.to(device)
         optimizer.zero_grad()
         z = model(data)
@@ -146,21 +161,29 @@ def train():
         pos_labels = torch.ones(pos_preds.shape[0]).to(device)
         neg_labels = torch.zeros(neg_preds.shape[0]).to(device)
 
-        loss = F.binary_cross_entropy_with_logits(pos_preds, pos_labels) + F.binary_cross_entropy_with_logits(neg_preds, neg_labels)
-        loss.backward()
-        optimizer.step()
+        try:
+            loss = F.binary_cross_entropy_with_logits(pos_preds, pos_labels) + F.binary_cross_entropy_with_logits(neg_preds, neg_labels)
+        except:
+            skipped += 1
+            continue
 
-        total_loss += loss.item()
+        if not math.isnan(loss.item()):
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        else:
+            skipped += 1
 
-    return total_loss / len(train_dataloader)
+    return total_loss / (len(train_dataset)-skipped)
 
 def validate():
     model.eval()
     preds = []
     ground_truths = []
+    skipped = 0
     with torch.no_grad():
         total_loss = 0
-        for data in valid_dataloader:
+        for data in valid_dataset:
             data = data.to(device)
             z = model(data)
             pos_edge_index, neg_edge_index = generate_positive_negative_examples(data['directlyConnectedTo'].edge_index, data['module'].num_nodes)
@@ -171,13 +194,20 @@ def validate():
             neg_labels = torch.zeros(neg_preds.shape[0]).to(device)
             ground_truths.append(torch.cat([pos_labels, neg_labels], dim=0))
 
-            loss = F.binary_cross_entropy_with_logits(pos_preds, pos_labels) + F.binary_cross_entropy_with_logits(neg_preds, neg_labels)
-            total_loss += loss.item()
+            try:
+                loss = F.binary_cross_entropy_with_logits(pos_preds, pos_labels) + F.binary_cross_entropy_with_logits(neg_preds, neg_labels)
+            except:
+                skipped += 1
+                continue
+            if not math.isnan(loss.item()):
+                total_loss += loss.item()
+            else:
+                skipped += 1
 
     pred = torch.cat(preds, dim=0)
     ground_truth = torch.cat(ground_truths, dim=0)
     auc = roc_auc_score(ground_truth, pred)
-    RocCurveDisplay.from_predictions(ground_truth, pred)
+    # RocCurveDisplay.from_predictions(ground_truth, pred)
 
     # plt.show()
     
@@ -185,12 +215,12 @@ def validate():
     acc = accuracy_score(ground_truth, pred)
     # TODO: für jeden data punkt (Feld)
 
-    loss = total_loss / len(valid_dataloader)
+    loss = total_loss / (len(valid_dataset)- skipped)
 
     return loss, auc, acc
 
 
-num_epochs = 50
+num_epochs = 10
 train_losses = []
 validation_accuracies = []
 validation_auc = []
@@ -203,6 +233,14 @@ for epoch in range(num_epochs):
     train_losses.append(train_loss)
 
     print(f"Epoch: {epoch+1}, Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}, Accuracy: {acc}, AUC: {auc}")
+
+torch.save({
+    'model_state_dict': model.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+    'train_losses': train_losses,
+    'validation_accuracies': validation_accuracies,
+    'validation_auc': validation_auc
+}, 'models/model_real_good_2.pt')
 
 
 # visualize training performance
